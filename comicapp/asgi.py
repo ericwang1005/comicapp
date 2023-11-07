098e7969f6e04683f4ca433de7bd8d087f65d5f9
Xuan Wang2 (Edwards Contingent Worker) <Xuan_Wang2@edwards.com>
2023-11-07 15:32:10 +0800

    創建comicapp

---
+++ comicapp/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for comicapp project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'comicapp.settings')
+
+application = get_asgi_application()
